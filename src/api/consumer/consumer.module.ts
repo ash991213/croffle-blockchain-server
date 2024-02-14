44602e5858e2da88d8b7f8fc7b36e1345a225cee
ash991213 <ads1213@nate.com>
2024-02-14 19:01:18 +0900

    AWS SQS 컨슈머 모듈 & 서비스 개발 완료

---
+++ src/api/consumer/consumer.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { SqsModule } from '@ssut/nestjs-sqs';
+
+import { CustomLoggerModule } from 'src/module/custom.logger.module';
+
+import { ConfigService } from '@nestjs/config';
+import { ConsumerService } from 'src/api/consumer/service/consumer.service';
+
+import * as AWS from 'aws-sdk';
+
+@Module({
+    imports: [
+        CustomLoggerModule,
+        SqsModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                AWS.config.update({
+                    region: configService.get<string>('AWS_REGION'),
+                    accessKeyId: configService.get<string>('AWS_ACCESS_KEY_ID'),
+                    secretAccessKey: configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+                });
+
+                return {
+                    consumers: [
+                        {
+                            name: configService.get<string>('AWS_SQS_NAME'),
+                            queueUrl: configService.get<string>('AWS_SQS_URL'),
+                            region: configService.get<string>('AWS_REGION'),
+                        },
+                    ],
+                };
+            },
+        }),
+    ],
+    providers: [ConsumerService],
+    exports: [ConsumerService],
+})
+export class ConsumerModule {}
---
+++ src/api/consumer/service/consumer.service.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { SqsMessageHandler } from '@ssut/nestjs-sqs';
+
+import { CustomLogger } from 'src/config/logger/custom.logger.config';
+
+import * as AWS from 'aws-sdk';
+
+@Injectable()
+export class ConsumerService {
+    constructor(
+        @Inject('CROFFLE_BLOCKCHAIN_SERVER_LOG')
+        private readonly logger: CustomLogger,
+    ) {}
+
+    @SqsMessageHandler(process.env.AWS_SQS_NAME, false)
+    async handleMessage(message: AWS.SQS.Message) {
+        const obj: any = JSON.parse(message.Body) as {
+            message: string;
+            date: string;
+        };
+
+        const { data } = JSON.parse(obj.Message);
+
+        console.log(data);
+    }
+}
